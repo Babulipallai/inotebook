7c6a085b9af86169c797e7d98ce69fd5e5893478
Babuli Pallai <babulipalai6@gmail.com>
2023-03-06 18:51:39 +0530

    Adding SignUp Component to iNotebook

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -20,19 +20,21 @@ router.post(
     }),
   ],
   async (req, res) => {
+    let success = false;
     // If there are errors, return bad request and the errors.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
     // Check whether the user with this email exists already
     try {
       let user = await User.findOne({ email: req.body.email });
       // console.log(user)
       if (user) {
-        return res
-          .status(400)
-          .json({ error: "Sorry a user with this email already exists" });
+        return res.status(400).json({
+          success,
+          error: "Sorry a user with this email already exists",
+        });
       }
       const salt = await bcrypt.genSalt(10);
       const secPass = await bcrypt.hash(req.body.password, salt);
@@ -49,10 +51,10 @@ router.post(
       };
 
       const authtoken = jwt.sign(data, JWT_SECRET);
-      // console.log(jwtData);
-      // res.json(user);
 
-      res.json({ authtoken });
+      // res.json(user);
+      success = true;
+      res.json({ success, authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some error occured");
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Signup = () => {
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+  const navigate = useNavigate();
+
+  const host = "http://localhost:5000";
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name, email, password } = credentials;
+    const response = await fetch(`${host}/api/auth/createuser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+    const json = await response.json();
+
+    localStorage.setItem("token", json.authtoken);
+    navigate("/");
+  };
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
   return (
     <div>
-      I am signup
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter name"
+            onChange={onChange}
+            name="name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={onChange}
+            name="email"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            onChange={onChange}
+            name="password"
+            minLength={5}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="cpassword">
+          <Form.Label>Confirm password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter confirm password"
+            onChange={onChange}
+            name="cpassword"
+            minLength={5}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
